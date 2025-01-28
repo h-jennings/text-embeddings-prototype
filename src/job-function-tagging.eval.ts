c0da9259a13c0508c11facd9e7959275e4424ae8
Hunter Jennings <jenningsdhunter@gmail.com>
2025-01-28 15:30:02 -0500

    feat(job-function-tagging): Add evaluation for job function tagging
    -  Implement evalite scorer for job function tags
    -  Create task to generate job function tags from job summary
    -  Use createScorer to evaluate correctness of generated tags

---
+++ src/job-function-tagging.eval.ts
@@ -0,0 +1,38 @@
+import { createScorer, evalite } from "evalite";
+import { data } from "./data/job-summary-test-data.js";
+import { createJobSummary } from "./processors/create-job-summary/create-job-summary.js";
+import { createEmbedding } from "./utils/create-embedding.js";
+import { getTags } from "./utils/get-tags.js";
+
+const includesExpectedTag = createScorer<{ jobFunctionTags: Array<string> }, Array<{ name: string; score: number }>>({
+  name: "Includes Expected Tags",
+  description: "The tags generated are correct.",
+  scorer: ({ output, input }) => {
+    const outputTags = output.map((tag) => tag.name);
+    const expectedTags = input.jobFunctionTags;
+    return expectedTags.every((tag) => outputTags.includes(tag)) ? 1 : 0;
+  },
+});
+
+evalite("Correctly tags a role with job function", {
+  data: () => {
+    return data.map((item) => {
+      return {
+        input: item,
+      };
+    });
+  },
+  task: async (input) => {
+    const summary = await createJobSummary(2, { title: input.title, description: input.description });
+    const embedding = await createEmbedding(summary);
+    const tags = await getTags(embedding);
+
+    return tags.map((tag) => {
+      return {
+        name: tag.name,
+        score: tag.score,
+      };
+    });
+  },
+  scorers: [includesExpectedTag],
+});
